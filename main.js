7c1213f9392c38abc83a85ed7549d7714b3692e4
over9613 <over9613@gmail.com>
2026-02-01 11:51:11 +0000

    feat: Add dark mode and generate 5 lotto sets

--- main.js
+++ main.js
@@ -2,18 +2,28 @@
 document.getElementById("generate-btn").addEventListener("click", () => {
     const resultContainer = document.getElementById("result");
     resultContainer.innerHTML = "";
-    const numbers = new Set();
-    while(numbers.size < 6) {
-        const randomNumber = Math.floor(Math.random() * 45) + 1;
-        numbers.add(randomNumber);
-    }
-    
-    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+    for (let i = 0; i < 5; i++) {
+        const numbers = new Set();
+        while(numbers.size < 6) {
+            const randomNumber = Math.floor(Math.random() * 45) + 1;
+            numbers.add(randomNumber);
+        }
+        
+        const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+        const numberSetElement = document.createElement("div");
+        numberSetElement.classList.add("lotto-number-set");
 
-    sortedNumbers.forEach(number => {
-        const numberElement = document.createElement("span");
-        numberElement.textContent = number;
-        numberElement.classList.add("lotto-number");
-        resultContainer.appendChild(numberElement);
-    });
+        sortedNumbers.forEach(number => {
+            const numberElement = document.createElement("span");
+            numberElement.textContent = number;
+            numberElement.classList.add("lotto-number");
+            numberSetElement.appendChild(numberElement);
+        });
+        resultContainer.appendChild(numberSetElement);
+    }
+});
+
+const themeSwitch = document.getElementById("checkbox");
+themeSwitch.addEventListener("change", () => {
+    document.body.classList.toggle("dark-mode");
 });
