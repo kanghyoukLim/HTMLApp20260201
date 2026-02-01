d5d1d4ef3821a93dc72c0ea32454083ca35a21cc
over9613 <over9613@gmail.com>
2026-02-01 11:29:43 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add JS here
+
+document.getElementById("generate-btn").addEventListener("click", () => {
+    const resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = "";
+    const numbers = new Set();
+    while(numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+    
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    sortedNumbers.forEach(number => {
+        const numberElement = document.createElement("span");
+        numberElement.textContent = number;
+        numberElement.classList.add("lotto-number");
+        resultContainer.appendChild(numberElement);
+    });
+});
